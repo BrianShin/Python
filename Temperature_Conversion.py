76b998a95cb4c7a2814caa2bcc69967cf3b6cb17
BrianShin <30308132+BrianShin@users.noreply.github.com>
2017-09-05 11:20:21 +0900

    Temperature_Converison
    Conversion of Celsius to Fahrenheit and vice versa

---
+++ Temperature_Conversion.py
@@ -0,0 +1,5 @@
+x = int(input('what is the temperature in Celsius today? '))
+print('temperate in fahrenheit is: ',x * 9 / 5 + 32)
+
+x = int(input('how tall are you centimeters? '))
+print('then in inches you are: ', x / 2.54)
